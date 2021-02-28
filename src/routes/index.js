dd80344b791d877502d7c6792fb3f528c120bc69
Aman Raj <amanr11314@gmail.com>
2021-02-28 21:05:28 +0530

    💡 initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,34 @@
+const courseController = require('../controllers/courseController')
+
+const routes = [
+    {
+        method: 'GET',
+        url: '/api/course',
+        handler: courseController.getCourse
+    },
+
+    {
+        method: 'GET',
+        url: '/api/course/:id',
+        handler: courseController.getSingleCourse
+    },
+    {
+        method: 'POST',
+        url: '/api/course',
+        handler: courseController.addCourse
+    },
+
+    {
+        method: 'PUT',
+        url: '/api/course/:id',
+        handler: courseController.updateCourse
+    },
+    {
+        method: 'DELETE',
+        url: '/api/course/:id',
+        handler: courseController.deleteourse
+    }
+
+]
+
+module.exports = routes
